a5c52b6a92e311d99dc739a5581dab2c6da9ec09
eamurray06 <eamurray@seas.upenn.edu>
2025-07-27 20:30:25 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  experimental: {
+    // Enable the experimental app directory (used below for the page component)
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
